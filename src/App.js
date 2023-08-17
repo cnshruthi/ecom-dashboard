7f19b09ea6cd3bf2f3beeec9c08c2bf26b788921
cnshruthi <cnshruthi123@gmail.com>
2023-08-17 10:23:38 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import {Button} from 'react-bootstrap'
 
 function App() {
+
+  const [count, setCount] = useState(1)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ecom - Dashboard Count:{count}</h1>
+      <button>React Button</button>
+      <Button onClick={()=>setCount(count+1)} >BootStrap Button</Button>
     </div>
   );
 }
